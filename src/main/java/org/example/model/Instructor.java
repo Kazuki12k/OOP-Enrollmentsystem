453d980b557b1e6fc93ea9b50a2e54a4f31c35c6
rafael <rafael.rebancos22@gmail.com>
2026-03-07 16:02:56 +0800

    V2

---
+++ src/main/java/org/example/model/Instructor.java
@@ -0,0 +1,35 @@
+package org.example.model;
+
+public class Instructor {
+    String InstructorID;
+    String InstructorName;
+    String Courses;
+
+    public Instructor(String ID, String Name, String Courses) {
+        InstructorID = ID;
+        InstructorName = Name;
+        this.Courses = Courses;
+    }
+
+    public void setInstructorID(){
+        this.InstructorID = InstructorID;
+    }
+    public String getInstructorID(){
+        return InstructorID;
+    }
+
+    public void setInstructorName() {
+        this.InstructorName = InstructorName;
+    }
+    public String getInstructorName() {
+        return InstructorName;
+    }
+
+    public void setCourses() {
+        this.Courses = Courses;
+    }
+    public String getCourses() {
+        return Courses;
+    }
+
+}
