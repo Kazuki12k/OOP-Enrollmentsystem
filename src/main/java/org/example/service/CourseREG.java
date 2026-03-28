d7628a26509924796a3d7d4cfe88e4bc954a49a6
rafael <rafael.rebancos22@gmail.com>
2026-03-28 16:30:36 +0800

    v2

---
+++ src/main/java/org/example/service/CourseREG.java
@@ -0,0 +1,10 @@
+package org.example.service;
+
+import org.example.model.Course;
+
+public interface CourseREG {
+    void addCourse(Course course);
+    void DisplayAll();
+    void updateCourse(Course course);
+    void deleteCourseRecord(Course course);
+}
--- src/main/java/org/example/service/StudentRegistration.java
+++ src/main/java/org/example/service/StudentRegistration.java
@@ -3,7 +3,7 @@ package org.example.service;
 import java.util.ArrayList;
 import org.example.model.Student;
 
-public class StudentRegistration {
+public class StudentRegistration implements StudentREG{
 
     private ArrayList<Student> studentlist = new ArrayList<Student>();
 
