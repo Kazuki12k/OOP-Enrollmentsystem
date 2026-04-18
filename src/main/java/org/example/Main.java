e90cb416480bae05830ecc0c94ac55e609b78ea7
rafael <rafael.rebancos22@gmail.com>
2026-04-18 11:43:55 +0800

    V4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.model.Course;
+import org.example.model.Instructor;
 import org.example.model.Student;
 import org.example.service.CourseRegistration;
 import org.example.service.Registar;
@@ -15,7 +16,8 @@ public class Main {
     public static void main(String[] args) {
 
 
-//        List<Course> courseList = new ArrayList<>();
+        List<Course> courseList = new ArrayList<>();
+        List<Instructor> instructorList = new ArrayList<>();
 //
 //        Course c1 = new Course(02, "Discrete Mathematics", "IT");
 //        Course c2 = new Course(79, "Information Management", "IT");
@@ -25,6 +27,7 @@ public class Main {
         registar.registerStudent(new Student(000123, "John Doe", "IT"));
         registar.registerStudent(new Student(000124, "Jane Doe", "IT"));
         registar.addCourse(new Course(00001, "Integrative Programming", "IT"));
+        registar.saveDepartment("CITE", "College of Information Technology and Engineering", instructorList);
 
         registar.displayAll();
         registar.displayALL();
